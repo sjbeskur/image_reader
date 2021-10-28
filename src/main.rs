e8aa2f89c410094a6ec0ba34c0e5dba4bea5097c
sjbeskur <sam.beskur@gmail.com>
2021-10-27 21:03:25 -0600

    unit tests / Box<dyn Error>

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,29 @@
 
 use std::env;
-use std::error::Error;
 use image::io::Reader as ImageReader;
 use std::time::Instant;
 
-fn main()  {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         println!("Invalid args");
         std::process::exit(1);
     }
-
     let file = &args[1];
 
-    let now = Instant::now();
-    let img = ImageReader::open(file).unwrap().decode().unwrap();
+    let mut now = Instant::now();
+    let img = read_image(file).expect(&format!("Failed to open open file {}", file));
     println!("Image read time: {}(millis)", now.elapsed().as_millis());  
-    //  1ms     =  1,000,000 ns	
-    //  0.1 ms  =    100,000 ns	
-    // 	0.01 ms =     10,000 ns
 
+    now = Instant::now();
     let mut bytes: Vec<u8> = Vec::new();
-    img.write_to(&mut bytes, image::ImageOutputFormat::Png).unwrap();
-    println!("Original image bytes: {}", bytes.len());
+    img.write_to(&mut bytes, image::ImageOutputFormat::Png)?;    
+    let original_bytes = bytes.len();
+    println!("Writing image bits to buffer in: {}(millis)", now.elapsed().as_millis());  
 
-
-    let now = Instant::now();
+    now = Instant::now();
 
     let threshold: u8 = 240;
-
     // filter the image u8 pixels and retrieve only the index and values 
     // where the values are greater than threshold
     let iv: Vec<_> = bytes.into_iter()
@@ -36,23 +31,32 @@ fn main()  {
                     .filter(|&(_,val)| val > threshold)
                     .map(|(index,val)| (index, val))
                     .collect();
+    println!("Original image bytes: {}", original_bytes);
+    println!("Filtered image bytes: {}", iv.len());
+    println!("Filter time: {}(millis)", now.elapsed().as_millis());     
 
-    println!("filtered image bytes: {}\n", iv.len());
-
-    println!("Filter time: {}(millis)", now.elapsed().as_millis());  
-    
-
-
+    Ok(())
 }
 
-fn sanity_check(){
-    let v = [true, false, true];
-    let result: Vec<_> = v.iter()
+
+fn read_image(file: &String) -> Result<image::DynamicImage, image::ImageError> {
+    let img_reader = ImageReader::open(file)?; //
+    let img = img_reader.decode()?; 
+    Ok(img)
+}
+
+
+#[test]
+fn sanity_check_vector_filter(){
+    let v = [true, false, true, true, false, true];
+    let result: Vec<_> = v.into_iter()
         .enumerate()
-        .filter(|&(_, &value)| !value )
+        .filter(|&(_, value)| value == true )
         .map(|(i, v)| (i,v))
         .collect();
 
     println!("{:?}", result);
-
+    let expected: Vec<(usize,bool)> = vec![ (0,true), (2,true),(3,true), (5,true)];
+    println!("{:?}", expected);
+    assert_eq!(result, expected);
 }
